ac8451c0ff0145af0af01e50eaa46b17fcad1176
Makmaer <instrojgrad@gmail.com>
2022-08-10 20:49:45 +0500

    Создаем массив и метод, считающий кол-во эл-тов в массиве из символов менее 3х

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int KolEllements(string[] array)
+{
+int length = array.Length;
+int lengthString = 3;
+int el = 0;
+int i =0;
+    while (i<length)
+    {
+        if (array[i].Length <= lengthString)
+        {
+            el++; i++;
+        } 
+        else i++;   
+    }
+return el;
+}
+
+
+string[] mas1 = {"fdf", "wert", "1", "4567"};
+System.Console.WriteLine(KolEllements(mas1)); 
